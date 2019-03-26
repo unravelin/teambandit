a85b91983ef72b3669d37f21cd5d7e0086ac529d
Sam Fitzpatrick <sam.fitzpatrick@ravelin.com>
2019-03-26 13:18:35 +0000

    update port

--- slack_app.py
+++ slack_app.py
@@ -328,7 +328,7 @@ def astridTestsThings():
     print(json.dumps(teamsObjects))
 
 if __name__ == '__main__':  
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 20129))
     app.run(host='0.0.0.0', port=port)
 
 
