f6518d7f4d3602161a2bc5cd5271e3b1b95a7db4
Sam Fitzpatrick <46376226+samfitz66@users.noreply.github.com>
2019-04-04 10:46:29 +0100

    Update Port

--- slack_app.py
+++ slack_app.py
@@ -266,7 +266,7 @@ def get_name_from_userid(userID):
 
 if __name__ == '__main__':  
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=port)
 
 
 
